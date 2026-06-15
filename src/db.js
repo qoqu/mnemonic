bd0efb224a78ca9d26f6135278887078235440e0
qoqu <qoqu@users.noreply.github.com>
2026-06-15 14:26:30 +0800

    fix: busy_timeout=5000 + test cleanup temp dir

--- src/db.js
+++ src/db.js
@@ -28,6 +28,7 @@ export function getDb() {
   db = new DatabaseSync(DB_PATH);
   db.exec('PRAGMA journal_mode = WAL');
   db.exec('PRAGMA foreign_keys = ON');
+  db.exec('PRAGMA busy_timeout = 5000');
 
   runMigrations(db);
   return db;
